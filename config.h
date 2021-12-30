03fa2c0e8b8d100b6243eb6e4522d49db6da131a
Christopher Curl <ccurl@mmm.com>
2021-12-30 16:40:46 -0500

    Cleanup

--- config.h
+++ config.h
@@ -18,7 +18,7 @@
 #ifdef __PC__
   #define STK_SZ          16
   #define LSTACK_SZ       8
-  #define USER_SZ        (64*1024)
+  #define USER_SZ        (256*1024)
   #define NUM_REGS       (26*26*26)
   #define NUM_FUNCS      (26*26*26)
 #else
